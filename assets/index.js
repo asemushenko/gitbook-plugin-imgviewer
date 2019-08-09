1466c90c9b342b762cd4acc55828eae6f7053787
Alena Semushenko <asemushenko@wallarm.com>
2019-08-09 18:48:10 +0700

    Add class

--- assets/index.js
+++ assets/index.js
@@ -1,14 +1,14 @@
-require(['gitbook'], function(gitbook) {
-  gitbook.events.bind('page.change', function() {
-    $('img').each(function(index, img) {
-      let currentImg = $(img);
-      currentImg.viewer({
-        navbar: false,
-        toolbar: false,
-        fullscreen: false,
-        loop: false,
-        rotatable: false,
-      });
+require(['gitbook'], function (gitbook) {
+    gitbook.events.bind('page.change', function () {
+        $('img').each(function (index, img) {
+            let currentImg = $(img);
+            currentImg.viewer({
+                navbar: false,
+                fullscreen: false,
+                loop: false,
+                rotatable: false,
+            });
+            currentImg.addClass('base-img');
+        });
     });
-  });
 });
